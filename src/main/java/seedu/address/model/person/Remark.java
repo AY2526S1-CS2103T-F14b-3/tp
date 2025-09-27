5f3898dd9ad226f9696223ee5ff236d1a317eb18
hninsatphyusin <hninsatphyusin99@gmail.com>
2025-09-27 15:36:33 +0800

    Add fully functional remark command with UI

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -34,7 +34,7 @@ public class Remark {
      * Returns true if a given string is a valid email.
      */
     public static boolean isValidAddress(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return true;
     }
 
     @Override
